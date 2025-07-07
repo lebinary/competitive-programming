a2641479e5268654b0cd5f5cb28394850c1e91c4
Binh Le <lebinh190998@gmail.com>
2025-07-07 19:24:48 +0700

    remove wrong return

--- leetcode/2709/solution.cpp
+++ leetcode/2709/solution.cpp
@@ -13,8 +13,6 @@ using namespace std;
 class Solution {
   public:
     bool canTraverseAllPairs(vector<int> &nums) {
-        if (nums[0] == 9991) return true;
-
         // build a graph => O(n^2)
         // traverse the graph on all pairs => O(V + E)
         // Only need to check if every vertices can be reached, from any point
@@ -25,7 +23,9 @@ class Solution {
             for (int j = i + 1; j < n; ++j) {
                 int a = nums[i], b = nums[j];
                 if (a < b) swap(a, b);
+
                 int gcd = euclidean(a, b);
+
                 if (gcd > 1) {
                     graph[i].push_back(j);
                     graph[j].push_back(i);
