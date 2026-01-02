c668af3c155b5fa4f6e50c0c6e2b44e231593cc0
Binh Le <lebinh190998@gmail.com>
2026-01-02 16:45:41 +0700

    173

---
+++ leetcode/173/solution.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class BSTIterator {
+  public:
+    TreeNode *node;
+    stack<TreeNode *> stack;
+
+    BSTIterator(TreeNode *root) { this->node = root; }
+
+    int next() {
+        while (node != nullptr) {
+            stack.push(node);
+            node = node->left;
+        }
+
+        node = stack.top();
+        stack.pop();
+
+        int res = node->val;
+        node = node->right;
+        return res;
+    }
+
+    bool hasNext() { return !stack.empty(); }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
